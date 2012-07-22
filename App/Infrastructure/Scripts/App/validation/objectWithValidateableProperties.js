f0a98d130336d8e17157c4939ff6041713fac16f
Andrew Davey <andrew@equin.co.uk>
2012-07-22 17:05:45 +0100

    Refactor

--- App/Infrastructure/Scripts/App/validation/objectWithValidateableProperties.js
+++ App/Infrastructure/Scripts/App/validation/objectWithValidateableProperties.js
@@ -1,13 +1,20 @@
 ﻿var objectWithValidateableProperties = {
-    validate: function() {
-        var allValid = true;
-        for (var propertyName in this) {
-            if (this[propertyName].validation) {
-                if (!this[propertyName].validation.validate()) {
-                    allValid = false;
-                }
-            }
-        }
-        return allValid;
+    validate: function () {
+        // Returns true if all properties of `this`, which have validation, are valid.
+        
+        var getProperty = function(key) { return this[key]; };
+        var hasValidation = function (property) { return property && property.validation && property.validation.validate; };
+        var callValidate = function (property) { return property.validation.validate(); };
+        var isNotValid = function (result) { return result !== true; };
+        
+        var errorCount = Object
+            .keys(this)
+            .map(getProperty, this)
+            .filter(hasValidation)
+            .map(callValidate)
+            .filter(isNotValid)
+            .length;
+
+        return errorCount === 0;
     }
 };
