46259473e55b32b49c4098641b93e2b74760ea75
Andrew Davey <andrew@equin.co.uk>
2012-07-31 15:55:33 +0100

    Add EventHub

---
+++ App/Infrastructure/Scripts/App/EventHub.js
@@ -0,0 +1,20 @@
+﻿var EventHub = Object.inherit({
+    init: function () {
+        this.handlers = { };
+    },
+    
+    subscribe: function (eventName, handlerFunction) {
+        if (this.handlers.hasOwnProperty(eventName)) {
+            this.handlers[eventName].push(handlerFunction);
+        } else {
+            this.handlers[eventName] = [handlerFunction];
+        }
+    },
+    
+    publish: function (eventName, eventData) {
+        var handlers = this.handlers[eventName] || [];
+        handlers.forEach(function(handler) {
+            handler(eventData);
+        });
+    }
+});
